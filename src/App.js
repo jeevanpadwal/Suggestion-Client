d404774a13f7a64d2c29d9a2fddf432806082d69
G1 <jeevanpadwal01@gmail.com>
2025-06-02 22:21:38 +0530

    new server

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ const EntertainmentApp = () => {
       };
 
       // Call backend API
-      const response = await fetch('https://suggestions-production-ab87.up.railway.app/api/recommend', {
+      const response = await fetch('https://suggestions-production-80b8.up.railway.app/api/recommend', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
